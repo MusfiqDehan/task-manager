ba6892f83bea26c70a950b53d21c6818e24aa8cf
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-09-18 12:28:24 +0600

    Create Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    is_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('task_detail', args=[str(self.id)])
