d6915a2ff4409c6d64034e1c7a866c441ba362e9
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-09-19 15:45:45 +0600

    Add staicfile url patterns

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from tasks.views import TaskListView
 from django.contrib.auth.decorators import login_required
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='home'),
@@ -31,3 +32,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
