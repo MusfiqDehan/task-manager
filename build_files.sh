561491f944c154178d528c3c11a27b6cbeb21ad6
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-10-03 19:29:21 +0600

    Update vercel build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip3 install -r requirements.txt
-python3 manage.py collectstatic
+echo "====== BUILD START ======"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "====== BUILD FINISH ======="
