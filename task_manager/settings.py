1ebef6f9fa0e2a0d642e0fedc8763e3f243ffff1
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-10-03 19:43:00 +0600

    Back to previous static settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Media files (Images)
 # MEDIA_URL = '/media/'
