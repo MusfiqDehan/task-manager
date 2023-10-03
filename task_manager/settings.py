f7a1e2bbe75546a7ddfd04a20858f6fbbd40ce2c
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-10-03 20:09:26 +0600

    Keeping setups only for render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Media files (Images)
 # MEDIA_URL = '/media/'
